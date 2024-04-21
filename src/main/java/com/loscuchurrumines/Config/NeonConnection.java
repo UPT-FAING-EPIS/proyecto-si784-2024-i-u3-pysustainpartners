9a2d6d5ddc9ffe4776385549a54382d5b534fe08
ximena-ortiz <xo2021071080@virtual.upt.pe>
2024-04-21 16:39:26 -0500

    config

--- src/main/java/com/loscuchurrumines/Config/NeonConnection.java
+++ src/main/java/com/loscuchurrumines/Config/NeonConnection.java
@@ -1,29 +1,32 @@
-package com.loscuchurrumines.Config;
+package com.loscuchurrumines.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class NeonConnection {
 
-    private static final String DATABASE_URL = "jdbc:postgresql://ep-small-haze-88087949.us-east-2.aws.neon.tech/dbSustainPartners";
-    private static final String DATABASE_USER = "ximena-ortiz";
-    private static final String DATABASE_PASSWORD = "neq9Gm5awICO";
-    
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String DATABASE_URL = dotenv.get("DATABASE_URL");
+    private static final String DATABASE_USER = dotenv.get("DATABASE_USER");
+    private static final String DATABASE_PASSWORD = dotenv.get(
+        "DATABASE_PASSWORD"
+    );
+
+    private NeonConnection() {
+        throw new IllegalStateException("Utility class");
     }
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+            return DriverManager.getConnection(
+                DATABASE_URL,
+                DATABASE_USER,
+                DATABASE_PASSWORD
+            );
         } catch (SQLException ex) {
-            ex.printStackTrace();
             return null;
         }
     }
-    
 }
